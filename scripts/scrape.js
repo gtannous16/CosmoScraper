7a8bbc952dd76a51495ad14eafd706357cd96210
Gigi Tannous <gtannous16@gmail.com>
2019-06-30 21:53:38 -0400

    added headline and note js and scrape js using cosmo magazine website

---
+++ scripts/scrape.js
@@ -0,0 +1,30 @@
+//scrape script
+//require request and cheerio
+var request = require("request");
+var cheerio = require("cheerio");
+
+var scrape = function (cb) {
+    request("https://www.cosmopolitan.com/", function(err, res, body){
+        var $ = cheerio.load(body);
+        
+        var articles = [];
+
+        $(".full-item").each(function(i, element){
+            var head =$(this).children(".full-item-title").text().trim();
+            var sum =$(this).children(".full-item-dek").text().trim();
+
+            if(head && sum){
+                var headNeat = head.replace(/(\r\n|\n|\r|\t|\s+)/gm, " ").trim();
+                var sumNeat = sum.replace(/(\r\n|\n|\r|\t|\s+)/gm, " ").trim();
+
+                var dataToAdd = {
+                    headline:headNeat,
+                    summary: sumNeat
+                };
+                articles.push(dataToAdd);
+            }
+        });
+        cb(articles);
+    });
+};
+module.exports = scrape;
